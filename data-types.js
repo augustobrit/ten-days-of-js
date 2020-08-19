05c61055a7970571462e3cd441df903dba6296e7
augustobrit <augustobrito@outlook.com>
2020-08-19 07:16:59 -0300

    day one and day two solved

---
+++ data-types.js
@@ -0,0 +1,30 @@
+// Data Types are Immutable and Objects
+// Number
+// String
+// Boolean
+// Symbol
+// Null
+// Undefined
+
+// Symbolic Numbers
+// Infinity, -Infinity and NaN
+
+// isSafeInteger Method
+// Return true or false based on IEEE-754
+
+// Number.MAX_SAFE_INTEGER and MIN_SAFE_INTEGER
+// return a max and min constant of a safe int
+
+function performOperation(secondInteger, secondDecimal, secondString) {
+  const firstInteger = 4;
+
+  const firstDecimal = 4.0;
+
+  const firstString = 'HackerRank ';
+
+  console.log(firstInteger + parseInt(secondInteger));
+
+  console.log(firstDecimal + parseFloat(secondDecimal));
+
+  console.log(firstString + secondString);
+}
