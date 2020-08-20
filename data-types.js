6efb22f92f10d924a5f6d409dc5e566d9b817066
augustobrit <augustobrito@outlook.com>
2020-08-20 20:37:19 -0300

    data types

--- data-types.js
+++ data-types.js
@@ -1,4 +1,5 @@
 // Data Types are Immutable and Objects
+
 // Number
 // String
 // Boolean
@@ -14,6 +15,36 @@
 
 // Number.MAX_SAFE_INTEGER and MIN_SAFE_INTEGER
 // return a max and min constant of a safe int
+
+// String
+// Immutable
+let myString = 'Augusto';
+let mySubString = myString.substr();
+let myConcat = myString.concat(mySubString);
+
+// Boolean
+// Logical entity true or false
+
+// Symbol Data Type
+// Unique and Immutable primitive value
+
+// Null Data Type
+// only one value: null
+
+// typeOf operator
+// determine the type associated with a variable
+
+// Naming
+// Valid variable Names
+var _daysCount;
+var MinimumCost;
+var page10;
+var Total_elements;
+
+// Coercion
+// number + string - number is coerced to a string
+// boolean + string - boolean is coerced to a string
+// number + boolean - boolean is coerced to a number
 
 function performOperation(secondInteger, secondDecimal, secondString) {
   const firstInteger = 4;
